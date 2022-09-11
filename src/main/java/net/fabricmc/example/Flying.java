20b30db167011c4d29aa04e7dd955c7b5b0b67c7
fluffysnaff <fluffysnaff@protonmail.com>
2022-09-11 21:00:11 +0200

    Increase boatfly speed

--- src/main/java/net/fabricmc/example/Flying.java
+++ src/main/java/net/fabricmc/example/Flying.java
@@ -1,6 +1,7 @@
 package net.fabricmc.example;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
 import static net.fabricmc.example.Util.CLIENT;
@@ -20,7 +21,7 @@ public class Flying
             }
             Vec3d velocity = vehicle.getVelocity();
             double motionY = CLIENT.options.sprintKey.isPressed() ? -0.5 : (CLIENT.options.jumpKey.isPressed() ? 0.5 : 0);
-            vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+            vehicle.setVelocity(new Vec3d(velocity.x * 2, motionY, velocity.z * 2));
         }
     }
 }
