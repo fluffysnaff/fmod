1b9636340fe63fb1692fba3f04daa4f1ba6f4210
fluffysnaff <fluffysnaff@protonmail.com>
2022-09-14 21:06:36 +0200

    Fix boatlfyl

--- src/main/java/net/fabricmc/example/Flying.java
+++ src/main/java/net/fabricmc/example/Flying.java
@@ -13,8 +13,10 @@ public class Flying
         {
             Entity vehicle = CLIENT.player.getVehicle();
             Vec3d velocity = vehicle.getVelocity();
+            double motionX = CLIENT.options.backKey.isPressed() ? 0.5 : (CLIENT.options.forwardKey.isPressed() ? -0.5 : 0);
             double motionY = CLIENT.options.sprintKey.isPressed() ? -0.5 : (CLIENT.options.jumpKey.isPressed() ? 0.5 : 0);
-            vehicle.setVelocity(new Vec3d(velocity.x * 2, motionY, velocity.z * 2));
+            double motionZ = CLIENT.options.rightKey.isPressed() ? -0.5 : (CLIENT.options.leftKey.isPressed() ? 0.5 : 0);
+            vehicle.setVelocity(new Vec3d(motionX, motionY, velocity.z + motionZ));
         }
     }
 }
