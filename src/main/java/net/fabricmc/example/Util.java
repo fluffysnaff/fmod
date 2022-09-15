0658374d9de6cf139e351b29fedffae707923697
fluffysnaff <fluffysnaff@protonmail.com>
2022-09-15 17:23:17 +0200

    Added bigdecimal

--- src/main/java/net/fabricmc/example/Util.java
+++ src/main/java/net/fabricmc/example/Util.java
@@ -3,24 +3,22 @@ package net.fabricmc.example;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Util
 {
     public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
     public static String commandPrefix = "^";
 
-    public static double roundToDirection(double val, double decimals)
+    public static BigDecimal roundToDirection(BigDecimal val, int decimals)
     {
-        var round = Math.pow(10.0, decimals);
-        var rounded = Math.round(val * round) / round;
-        var dir = Math.nextAfter(rounded, rounded  + Math.signum(val));
-        return Math.round(dir * round) / round;
+        return val.setScale(decimals, RoundingMode.FLOOR);
     }
 
     public static void log(String msg)
     {
-        if(CLIENT != null)
-        {
-            CLIENT.player.sendMessage(Text.of("§5[FMod]: " + msg), false);
-        }
+        assert CLIENT.player != null;
+        CLIENT.player.sendMessage(Text.of("§5[FMod]: " + msg), false);
     }
 }
--- src/main/java/net/fabricmc/example/mixin/ClientConnectionMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ClientConnectionMixin.java
@@ -1,17 +1,19 @@
 package net.fabricmc.example.mixin;
-import net.fabricmc.example.FMod;
+
+import net.fabricmc.example.Bypass;
+
 import net.fabricmc.example.Util;
 import net.fabricmc.example.Vars;
-
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.Packet;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.math.BigDecimal;
 
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin
@@ -19,76 +21,6 @@ public class ClientConnectionMixin
     @Inject(at = @At("TAIL"), method = "send(Lnet/minecraft/network/Packet;)V", cancellable = true)
     public void send(Packet<?> packet, CallbackInfo ci)
     {
-        if(!Vars.bypassLo)
-        {
-            return;
-        }
-
-        if (packet instanceof PlayerMoveC2SPacket movePacket)
-        {
-            var movePacketAccessor = (PlayerMoveC2SPacketAccessor) movePacket;
-
-            double x = Util.roundToDirection(movePacketAccessor.getX(), 2.0);
-            double z = Util.roundToDirection(movePacketAccessor.getZ(), 2.0);
-
-            if((long)((x * 1000)) % 10 != 0 && (long)((z * 1000)) % 10 != 0)
-            {
-                x = Util.roundToDirection(movePacketAccessor.getX(), 1.0);
-                z = Util.roundToDirection(movePacketAccessor.getZ(), 1.0);
-            }
-
-            if((long)((x * 1000)) % 10 != 0 && (long)((z * 1000)) % 10 != 0)
-            {
-                x = Util.roundToDirection(movePacketAccessor.getX(), 0);
-                z = Util.roundToDirection(movePacketAccessor.getZ(), 0);
-            }
-            movePacketAccessor.setX(x);
-            movePacketAccessor.setZ(z);
-
-            double pitch = Util.roundToDirection(movePacketAccessor.getPitch(), 2.0);
-            double yaw = Util.roundToDirection(movePacketAccessor.getYaw(), 2.0);
-            if((long)((pitch * 1000)) % 10 != 0 && (long)((yaw * 1000)) % 10 != 0)
-            {
-                pitch = Util.roundToDirection(movePacketAccessor.getPitch(), 1.0);
-                yaw = Util.roundToDirection(movePacketAccessor.getYaw(), 1.0);
-            }
-
-            if((long)((pitch * 1000)) % 10 != 0 && (long)((yaw * 1000)) % 10 != 0)
-            {
-                pitch = Util.roundToDirection(movePacketAccessor.getPitch(), 0);
-                yaw = Util.roundToDirection(movePacketAccessor.getYaw(), 0);
-            }
-            movePacketAccessor.setPitch((float)pitch);
-            movePacketAccessor.setYaw((float)yaw);
-            FMod.LOGGER.info(String.format("XZPY: %f, %f, %f, %f", x, z, pitch, yaw));
-        }
-
-        if (packet instanceof VehicleMoveC2SPacket movePacket)
-        {
-            var vehiclePacketAccessor = (VehicleMoveC2SPacketAccessor) movePacket;
-
-            double x = Util.roundToDirection(vehiclePacketAccessor.getX(), 2.0);
-            double z = Util.roundToDirection(vehiclePacketAccessor.getZ(), 2.0);
-
-            if((long)(x * 1000) % 10 != 0 && (long)(z * 1000) % 10 != 0)
-            {
-                x = Util.roundToDirection(vehiclePacketAccessor.getX(), 1.0);
-                z = Util.roundToDirection(vehiclePacketAccessor.getZ(), 1.0);
-            }
-            vehiclePacketAccessor.setX(x);
-            vehiclePacketAccessor.setZ(z);
-
-            double pitch = Util.roundToDirection(vehiclePacketAccessor.getPitch(), 2.0);
-            double yaw = Util.roundToDirection(vehiclePacketAccessor.getYaw(), 2.0);
-            if(((long)(pitch * 1000)) % 10 != 0 && ((long)(yaw * 1000)) % 10 != 0)
-            {
-                pitch = Util.roundToDirection(vehiclePacketAccessor.getPitch(), 1.0);
-                yaw = Util.roundToDirection(vehiclePacketAccessor.getYaw(), 1.0);
-            }
-
-            vehiclePacketAccessor.setPitch((float)pitch);
-            vehiclePacketAccessor.setYaw((float)yaw);
-            FMod.LOGGER.info(String.format("XZPY: %f, %f, %f, %f", x, z, pitch, yaw));
-        }
+        Bypass.MovementBypass(packet, ci);
     }
 }
