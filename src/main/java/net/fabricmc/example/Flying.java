1bc46bc75aba5db86cf429d28f781a7c4920157a
fluffysnaff <fluffysnaff@protonmail.com>
2022-09-15 17:23:24 +0200

    Fixing flying

--- src/main/java/net/fabricmc/example/Flying.java
+++ src/main/java/net/fabricmc/example/Flying.java
@@ -9,11 +9,13 @@ public class Flying
 {
     public static void tick()
     {
-        if(CLIENT.player != null && CLIENT.player.hasVehicle() && Vars.boatFly)
+        assert CLIENT.player != null;
+        if(CLIENT.player.hasVehicle() && Vars.boatFly)
         {
             Entity vehicle = CLIENT.player.getVehicle();
+            assert vehicle != null;
             Vec3d velocity = vehicle.getVelocity();
-            double motionX = CLIENT.options.backKey.isPressed() ? 0.5 : (CLIENT.options.forwardKey.isPressed() ? -0.5 : 0);
+            double motionX = CLIENT.options.forwardKey.isPressed() ? -0.5 : (CLIENT.options.backKey.isPressed() ? 0.5 : 0);
             double motionY = CLIENT.options.sprintKey.isPressed() ? -0.5 : (CLIENT.options.jumpKey.isPressed() ? 0.5 : 0);
             double motionZ = CLIENT.options.rightKey.isPressed() ? -0.5 : (CLIENT.options.leftKey.isPressed() ? 0.5 : 0);
             vehicle.setVelocity(new Vec3d(motionX, motionY, velocity.z + motionZ));
